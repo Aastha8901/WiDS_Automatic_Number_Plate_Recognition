5291571e94541e45c3902e4449d584142a0d4cfa
Anish <21b090003@iitb.ac.in>
2023-01-28 21:02:59 +0530

    hello

--- Pipline_model.py
+++ Pipline_model.py
@@ -1,21 +1,21 @@
 import tensorflow as tf
 import cv2
 import numpy as np
-import plotly as px
+import plotly.express as px
 
 
 model = tf.keras.models.load_model('./object_detection.h5')
 print('done')
 
-path = '../images/N16.jpeg'
+path = 'images/N16.jpeg'
 test_image = cv2.imread(path)
 test_image = np.array(test_image, dtype=np.uint8)
-test_image1 = tf.keras.utils.load_img(test_image, target_size=(224,224))
+test_image1 = tf.keras.utils.load_img(path, target_size=(224,224))
 test_image1_array = tf.keras.utils.img_to_array(test_image1)/255.0
 h,w,d = test_image.shape
 
 test_figure = px.imshow(test_image)
-test_figure.update_layout(width = 700, height = 500, margin = dict(l=10, r=10, b=10, t=10), xaxsis_title="Image 16")
+test_figure.update_layout(width = 700, height = 500, margin = dict(l=10, r=10, b=10, t=10), xaxis_title="Image 16")
 
 test_array = test_image1_array.reshape(1,224,224,3)
 
